845ddf0828e55f120dd567eb8e87cd9c1468465a
Thabang <thabangmariks@gmail.com>
2022-09-21 20:02:33 +0200

    index

--- manage .php
+++ manage .php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
 <html>
-<?php
-//session start
-session_start();
-?>
 
 <head>
     <!-- Basic -->
@@ -81,38 +77,22 @@ session_start();
 
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav">
-                                <?php
-                                if (isset($_SESSION['userName'])) {
-                                ?>
-                                    <li class="nav-item">
-                                        <a class="nav-link pl-lg-0" href="index.php">Home </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="service.html">Services</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="about.html"> About</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="why.html">Why Us</a>
-                                    </li>
-
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="contacts.html">contact</a>
-                                    </li>
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="signup.php">new User<span class="sr-only">(current)</span>
-                                        </a>
-                                    </li>
-                                <?php
-                                } else {
-                                ?>
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="login.php">login</a>
-                                    </li>
-                                <?php
-                                }
-                                ?>
+                                <li class="nav-item">
+                                    <a class="nav-link pl-lg-0" href="index.php">Home </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="service.html">Services</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="about.html"> About</a>
+                                </li>
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="why.html">Why Us <span class="sr-only">(current)</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.html">Contact Us</a>
+                                </li>
                             </ul>
                             <from class="search_form">
                                 <input type="text" class="form-control" placeholder="Search here...">
@@ -128,63 +108,67 @@ session_start();
         <!-- end header section -->
     </div>
 
-    <!-- contact section -->
-    <section class="contact_section layout_padding">
+    <!-- why us section -->
 
+    <section class="why_us_section layout_padding">
         <div class="container">
             <div class="heading_container heading_center">
                 <h2>
-                    Sign Up
+                    Why Choose Us
                 </h2>
             </div>
             <div class="row">
-                <div class="col-md-9 mx-auto">
-                    <div class="form_container">
-                        <form action="php/newUser.php" method="post">
-                            <div class="form-group col-md-6">
-                                <label for="type">UserType:</label>
-
-                                <select id="type" name="type" required>
-                                    <option value="1">Admin</option>
-                                    <option value="2">Technician</option>
-                                    <option value="3">Customer</option>
-                                </select>
-                            </div>
-                            <div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <input type="text" class="form-control" name="name" id="name" placeholder="First Name" required />
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <input type="text" class="form-control" name="lastname" id="lastname" placeholder="Last Name" required />
-                                </div>
-                            </div>
-                            <div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <input type="email" id="email" name="email" class="form-control" placeholder="Email" required />
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <input type="text" class="form-control" name="phone" placeholder="Phone Number" id="phone" required />
-                                </div>
-                            </div>
-                            <div class=" form-group ">
-                                <label for="" class=" form-label">Password</label>
-                                <input type="password" class="form-control" name="password" id="txtNewPassword" placeholder="" required>
-                            </div>
-                            <div class="form-group ">
-                                <label for="" class="form-label">Confirm Password</label>
-                                <input type="password" class="form-control" name="" id="txtConfirmPassword" placeholder="" required>
-                                <div id="divCheckPasswordMatch"></div>
-                            </div>
-                            <div class="btn-box">
-                                <button type="submit">Submit</button>
-                            </div>
-                        </form>
+                <div class="col-md-4">
+                    <div class="box ">
+                        <div class="img-box">
+                            <img src="images/w1.png" alt="">
+                        </div>
+                        <div class="detail-box">
+                            <h5>
+                                Free Diagnostics
+                            </h5>
+                            <p>
+                                variations of passages of Lorem Ipsum available
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box ">
+                        <div class="img-box">
+                            <img src="images/w2.png" alt="">
+                        </div>
+                        <div class="detail-box">
+                            <h5>
+                                Quick Repair Process
+                            </h5>
+                            <p>
+                                variations of passages of Lorem Ipsum available
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box ">
+                        <div class="img-box">
+                            <img src="images/w3.png" alt="">
+                        </div>
+                        <div class="detail-box">
+                            <h5>
+                                Low Price Guarantee
+                            </h5>
+                            <p>
+                                variations of passages of Lorem Ipsum available
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <!-- end contact section -->
+
+
+    <!-- end why us section -->
 
     <!-- footer section -->
     <footer class="footer_section">
@@ -312,21 +296,5 @@ session_start();
     <!-- End Google Map -->
 
 </body>
-<script>
-    //password verification
-    function checkPasswordMatch() {
-        var password = $("#txtNewPassword").val();
-        var confirmPassword = $("#txtConfirmPassword").val();
-
-        if (password != confirmPassword)
-            $("#divCheckPasswordMatch").html("<span style = 'color: red;'>Passwords do not match!</span>");
-        else
-            $("#divCheckPasswordMatch").html("<span style = 'color: green;'> Passwords match. </span>");
-    }
-
-    $(document).ready(function() {
-        $("#txtConfirmPassword").keyup(checkPasswordMatch);
-    });
-</script>
 
 </html>
