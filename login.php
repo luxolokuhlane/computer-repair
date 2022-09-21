e441bc70479aa49226d59140f52e3a25185fc1db
Luxolo Kuhlane <g18L5754@campus.ru.ac.za>
2022-09-21 18:49:48 +0200

    LO

--- login.php
+++ login.php
@@ -1,12 +1,43 @@
-!<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-    
-    </body>
-</html>
+<<?php
+
+//config 
+$host = "localhost";
+
+//database name
+$db = "test";
+
+//database username
+$user = "root";
+
+//database password
+$pass = "";
+
+//connection
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+//check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+//get data from form
+$fname = $_POST['fname'];
+
+$lname = $_POST['lname'];
+
+$email = $_POST['email'];
+
+$pass = $_POST['pass'];
+
+//insert data to database
+$sql = "INSERT INTO users (fname, lname, email, pass) VALUES ('$fname', '$lname', '$email', '$pass')";
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+
+?>
